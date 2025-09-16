1cdb79617f2df1ff71c3a7d41e5a874a446342b5
ImWojtek <wawrzynskiwojciech@gmail.com>
2025-09-16 23:08:22 +0200

    Player movement added

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,8 +22,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill((0, 0, 0))  # Fill the screen with black
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # Amount of seconds between each loop
