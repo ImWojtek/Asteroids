e5c671527eeca2264772871814904720ba97e6d5
ImWojtek <wawrzynskiwojciech@gmail.com>
2025-09-16 22:39:26 +0200

    player drawing and hitbox added

--- main.py
+++ main.py
@@ -3,10 +3,16 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,10 +21,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fill the screen with black
-
-
-
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # Amount of seconds between each loop
 
 
 if __name__ == "__main__":
